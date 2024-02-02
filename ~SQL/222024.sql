46727960e380e5bc8892bf1029159b93ca0657ee
Mat-tiw <tiweymain@gmail.com>
2024-02-02 20:33:35 +0700

    update database

--- ~SQL/222024.sql
+++ ~SQL/222024.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `artcrit_alpha` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
-USE `artcrit_alpha`;
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: artcrit_alpha
@@ -30,10 +28,13 @@ CREATE TABLE `ac_image` (
   `image_path` varchar(255) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_image`),
   KEY `post_id_idx` (`post_id`),
+  KEY `fk_user_id` (`user_id`),
+  CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `ac_user` (`id_user`),
   CONSTRAINT `post_id` FOREIGN KEY (`post_id`) REFERENCES `ac_post` (`id_post`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=74 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=81 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,7 +43,7 @@ CREATE TABLE `ac_image` (
 
 LOCK TABLES `ac_image` WRITE;
 /*!40000 ALTER TABLE `ac_image` DISABLE KEYS */;
-INSERT INTO `ac_image` VALUES (54,37,'http://localhost:3030/static/56fb62309aimage-1.jpg','2024-01-16 03:50:26','2024-01-16 03:50:26'),(55,37,'http://localhost:3030/static/902d0fc3fcimage-2.jpg','2024-01-16 03:50:26','2024-01-16 03:50:26'),(56,37,'http://localhost:3030/static/4a11530bbaimage-4.jpg','2024-01-16 03:50:26','2024-01-16 03:50:26'),(57,37,'http://localhost:3030/static/fb12d64e25image-3.jpg','2024-01-16 03:50:26','2024-01-16 03:50:26'),(58,38,'http://localhost:3030/static/6bd54ca3abimage-6.jpg','2024-01-16 03:51:23','2024-01-16 03:51:23'),(59,38,'http://localhost:3030/static/2b8355a6a8image-7.jpg','2024-01-16 03:51:23','2024-01-16 03:51:23'),(60,39,'http://localhost:3030/static/61af0d7cf8image-9.jpg','2024-01-16 03:53:05','2024-01-16 03:53:05'),(62,42,'http://localhost:3030/static/3d99072c88image-5.jpg','2024-01-16 04:17:48','2024-01-16 04:17:48'),(63,43,'http://localhost:3030/static/9ad703a30aimage-2.jpg','2024-01-16 07:26:38','2024-01-16 07:26:38'),(64,43,'http://localhost:3030/static/7a9832898fimage-3.jpg','2024-01-16 07:26:38','2024-01-16 07:26:38'),(65,43,'http://localhost:3030/static/dea31fa3a3image-4.jpg','2024-01-16 07:26:38','2024-01-16 07:26:38'),(66,43,'http://localhost:3030/static/5edd0c8191image-1.jpg','2024-01-16 07:26:38','2024-01-16 07:26:38'),(67,44,'http://localhost:3030/static/d4ed47e7f6image-9.jpg','2024-01-21 10:25:04','2024-01-21 10:25:04'),(68,44,'http://localhost:3030/static/f31fcceab9image-7.jpg','2024-01-21 10:25:04','2024-01-21 10:25:04'),(69,44,'http://localhost:3030/static/ac2e71242dimage-8.jpg','2024-01-21 10:25:04','2024-01-21 10:25:04'),(70,44,'http://localhost:3030/static/d67f4dd388image-6.jpg','2024-01-21 10:25:04','2024-01-21 10:25:04'),(71,45,'http://localhost:3030/static/98b32b38b8image-3.jpg','2024-01-24 13:12:31','2024-01-24 13:12:31'),(72,45,'http://localhost:3030/static/da6ba23f5eimage-4.jpg','2024-01-24 13:12:31','2024-01-24 13:12:31'),(73,45,'http://localhost:3030/static/17cb227a01image-2.jpg','2024-01-24 13:12:31','2024-01-24 13:12:31');
+INSERT INTO `ac_image` VALUES (54,37,'http://localhost:3030/static/56fb62309aimage-1.jpg','2024-01-16 03:50:26','2024-01-16 03:50:26',3),(55,37,'http://localhost:3030/static/902d0fc3fcimage-2.jpg','2024-01-16 03:50:26','2024-01-16 03:50:26',3),(56,37,'http://localhost:3030/static/4a11530bbaimage-4.jpg','2024-01-16 03:50:26','2024-01-16 03:50:26',3),(57,37,'http://localhost:3030/static/fb12d64e25image-3.jpg','2024-01-16 03:50:26','2024-01-16 03:50:26',3),(58,38,'http://localhost:3030/static/6bd54ca3abimage-6.jpg','2024-01-16 03:51:23','2024-01-16 03:51:23',3),(59,38,'http://localhost:3030/static/2b8355a6a8image-7.jpg','2024-01-16 03:51:23','2024-01-16 03:51:23',3),(60,39,'http://localhost:3030/static/61af0d7cf8image-9.jpg','2024-01-16 03:53:05','2024-01-16 03:53:05',3),(62,42,'http://localhost:3030/static/3d99072c88image-5.jpg','2024-01-16 04:17:48','2024-01-16 04:17:48',3),(63,43,'http://localhost:3030/static/9ad703a30aimage-2.jpg','2024-01-16 07:26:38','2024-01-16 07:26:38',4),(64,43,'http://localhost:3030/static/7a9832898fimage-3.jpg','2024-01-16 07:26:38','2024-01-16 07:26:38',4),(65,43,'http://localhost:3030/static/dea31fa3a3image-4.jpg','2024-01-16 07:26:38','2024-01-16 07:26:38',4),(66,43,'http://localhost:3030/static/5edd0c8191image-1.jpg','2024-01-16 07:26:38','2024-01-16 07:26:38',4),(67,44,'http://localhost:3030/static/d4ed47e7f6image-9.jpg','2024-01-21 10:25:04','2024-01-21 10:25:04',5),(68,44,'http://localhost:3030/static/f31fcceab9image-7.jpg','2024-01-21 10:25:04','2024-01-21 10:25:04',5),(69,44,'http://localhost:3030/static/ac2e71242dimage-8.jpg','2024-01-21 10:25:04','2024-01-21 10:25:04',5),(70,44,'http://localhost:3030/static/d67f4dd388image-6.jpg','2024-01-21 10:25:04','2024-01-21 10:25:04',5),(71,45,'http://localhost:3030/static/98b32b38b8image-3.jpg','2024-01-24 13:12:31','2024-01-24 13:12:31',1),(72,45,'http://localhost:3030/static/da6ba23f5eimage-4.jpg','2024-01-24 13:12:31','2024-01-24 13:12:31',1),(73,45,'http://localhost:3030/static/17cb227a01image-2.jpg','2024-01-24 13:12:31','2024-01-24 13:12:31',1),(74,46,'http://localhost:3030/static/3d5cb7ee87layer1.png','2024-01-31 04:52:09','2024-01-31 04:52:09',1),(75,46,'http://localhost:3030/static/9d39817264layer2.png','2024-01-31 04:52:09','2024-01-31 04:52:09',1),(76,46,'http://localhost:3030/static/f2de1717e3layer3.png','2024-01-31 04:52:09','2024-01-31 04:52:09',1),(77,46,'http://localhost:3030/static/1ac9656f2clayer4.png','2024-01-31 04:52:09','2024-01-31 04:52:09',1),(78,47,'http://localhost:3030/static/86f0865151image-1.jpg','2024-02-01 02:49:31','2024-02-01 02:49:31',1),(79,48,'http://localhost:3030/static/f187a791f3Illustration.png','2024-02-02 13:01:02','2024-02-02 13:01:02',1),(80,49,'http://localhost:3030/static/214f94053bay.png','2024-02-02 13:03:03','2024-02-02 13:03:03',1);
 /*!40000 ALTER TABLE `ac_image` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -63,7 +64,7 @@ CREATE TABLE `ac_post` (
   PRIMARY KEY (`id_post`),
   KEY `user_id_idx` (`user_id`),
   CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `ac_user` (`id_user`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -72,7 +73,7 @@ CREATE TABLE `ac_post` (
 
 LOCK TABLES `ac_post` WRITE;
 /*!40000 ALTER TABLE `ac_post` DISABLE KEYS */;
-INSERT INTO `ac_post` VALUES (37,'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua','test',3,'2024-01-16 03:50:26','2024-01-16 03:50:26'),(38,'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor i','testing',3,'2024-01-16 03:51:23','2024-01-16 03:51:23'),(39,'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor i','test',3,'2024-01-16 03:53:05','2024-01-16 03:53:05'),(42,'asd','asd',3,'2024-01-16 04:17:48','2024-01-16 04:17:48'),(43,'sasdads','adsf',4,'2024-01-16 07:26:38','2024-01-16 07:26:38'),(44,'faf','agag',5,'2024-01-21 10:25:04','2024-01-21 10:25:04'),(45,'asd','asd',1,'2024-01-24 13:12:31','2024-01-24 13:12:31');
+INSERT INTO `ac_post` VALUES (37,'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua','landscape',3,'2024-01-16 03:50:26','2024-01-16 03:50:26'),(38,'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor i','testing',3,'2024-01-16 03:51:23','2024-01-16 03:51:23'),(39,'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor i','test',3,'2024-01-16 03:53:05','2024-01-16 03:53:05'),(42,'asd','asd',3,'2024-01-16 04:17:48','2024-01-16 04:17:48'),(43,'sasdads','adsf',4,'2024-01-16 07:26:38','2024-01-16 07:26:38'),(44,'faf','agag',5,'2024-01-21 10:25:04','2024-01-21 10:25:04'),(45,'asd','asd',1,'2024-01-24 13:12:31','2024-01-24 13:12:31'),(46,'Lorem ipsum dolor sit amet, consectetur adipiscing elit','Anatomy',1,'2024-01-31 04:52:09','2024-01-31 04:52:09'),(47,'test title','landscape',1,'2024-02-01 02:49:31','2024-02-01 02:49:31'),(48,'asd','asd',1,'2024-02-02 13:01:01','2024-02-02 13:01:01'),(49,'asd','asd',1,'2024-02-02 13:03:03','2024-02-02 13:03:03');
 /*!40000 ALTER TABLE `ac_post` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -114,4 +115,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-01-28 12:16:41
+-- Dump completed on 2024-02-02 20:32:05
