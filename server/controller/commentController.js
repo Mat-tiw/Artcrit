39599ef05037b9ccf93ecd6a6a957bb0aae70909
Mat-tiw <99158892+Mat-tiw@users.noreply.github.com>
2024-02-09 13:42:38 +0700

    temp comment function

---
+++ server/controller/commentController.js
@@ -0,0 +1,57 @@
+import Comment from '../model/Comment.js'
+import Image from "../model/Image.js";
+import { fileURLToPath } from "url";
+import multer from "multer";
+import crypto from "crypto";
+import { dirname } from "path";
+
+function generateRandomString(length) {
+    return crypto.randomBytes(length).toString("hex");
+  }
+  const __filename = fileURLToPath(import.meta.url);
+  const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      const uploadPath = "D:/Desktop/artcrit-alpha/artcrit-early/server/public";
+      cb(null, uploadPath);
+    },
+    filename: function (req, file, cb) {
+      const randomString = generateRandomString(5);
+      cb(null, `${randomString}${file.originalname}`);
+    },
+  });
+  
+  const upload = multer({ storage });
+
+export const getAllComment = async(req,res)=>{
+    try {
+        const data = await Comment.findAll()
+        res.status(200).json({message:"fetch compete"})
+    } catch (error) {
+        res.status(500).json({message:"internal server error"})
+    }
+}
+
+export const addComment = async(req,res)=>{
+    const id = params.id
+    const {commentContent,userId} = req.body
+    const files = req.files
+    try {
+        const data = await Comment.create({
+            comment_content:commentContent,
+            user_id = userId,
+            post_id = id
+        })
+        const imagePromises = files.map(async(image)=>{
+          return await Image.create({
+            image_path: `http://localhost:3030/static/${image.filename}`,
+            comment_id:data.id_comment,
+            user_id:userId
+          })
+        });
+        await Promise.all(imagePromises);
+        res.status(200).json({message:"comment added"})
+    } catch (error) {
+        res.status(500).json({message:"internal server error"})
+    }
+}
+export const uploadCommentArray = upload.array("files",4)
--- server/router/router.js
+++ server/router/router.js
@@ -2,6 +2,8 @@ import express from "express";
 import { allUser,addUser,loginUser,findUser,updateUser,updateUserAvatar } from "../controller/userController.js";
 import {testFile,uploadPostImage,createPost,getAllPost,getPost,getUserPost} from "../controller/postController.js";
 import { getAllUserImage } from "../controller/imageController.js";
+import { addComment,uploadCommentArray } from "../controller/commentController.js";
+
 const router = express.Router();
 
 router.get("/user", allUser);
@@ -14,7 +16,9 @@ router.post('/post/create',uploadPostImage,createPost);
 router.post('/post/test',testFile);
 router.get('/post',getAllPost);
 router.get('/post/get/:id', getPost);
-router.get('/post/user/:id',getUserPost)
+router.get('/post/user/:id',getUserPost);
 
-router.get('/image/:id',getAllUserImage)
+router.get('/image/:id',getAllUserImage);
+
+router.post('/comment/add/:id',uploadCommentArray,addComment);
 export default router;
