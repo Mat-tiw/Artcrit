b847837a8bf9ac642439c2a234e6257afeae8265
Mat-tiw <tiweymain@gmail.com>
2024-02-09 11:29:34 +0700

    wip comment

---
+++ server/model/Comment.js
@@ -0,0 +1,31 @@
+import { Sequelize, DataTypes } from "sequelize";
+import sequelize from "../config/dbconfig.js";
+
+const Comment = sequelize.define("ac_comment", {
+  id_comment: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  comment_content: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  created_at: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
+  updated_at: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
+},{
+    Sequelize,
+    modelName:"Comment",
+    tableName:"ac_comment",
+    timestamps:true,
+    createdAt: "created_at",
+    updatedAt: "updated_at",
+    id: "id_comment",
+});
+export default Comment
--- server/model/Setup.js
+++ server/model/Setup.js
@@ -1,6 +1,8 @@
 import Post from "./Post.js";
 import Image from "./Image.js";
 import User from './User.js'
+import Comment from "./Comment.js";
+
 
 User.hasMany(Post,{
   foreignKey:'user_id',
@@ -10,6 +12,12 @@ User.hasMany(Image,{
   foreignKey:'user_id',
   sourceKey:'id_user'
 })
+User.hasMany(Comment,{
+  foreignKey:'user_id',
+  sourceKey:'id_user'
+})
+
+
 Post.belongsTo(User,{
   foreignKey:'user_id',
   targetKey:'id_user'
@@ -18,11 +26,27 @@ Post.hasMany(Image, {
   foreignKey: 'post_id',
   sourceKey: 'id_post'
 });
+Post.hasMany(Comment,{
+  foreignKey: 'post_id',
+  sourceKey: 'id_post'
+})
+
+
 Image.belongsTo(Post, {
   foreignKey: 'post_id',
   targetKey: 'id_post'
 });
 Image.belongsTo(User,{
+  foreignKey:'user_id',
+  targetKey: 'id_user'
+})
+
+
+Comment.belongsTo(Post,{
+  foreignKey: 'post_id',
+  targetKey: 'id_post'
+})
+Comment.belongsTo(User,{
   foreignKey:'user_id',
   targetKey: 'id_user'
 })
